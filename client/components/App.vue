2761502f1e41ab636f74a3c73cffd89fc842751f
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2017-04-16 22:05:49 +0100

    use bootstrap

--- client/components/App.vue
+++ client/components/App.vue
@@ -5,23 +5,7 @@
 </template>
 
 <style>
-body {
-  margin: 0;
-  font-size: 2rem;
-  font-family: -apple-system, BlinkMacSystemFont,
-               'avenir next', avenir,
-               helvetica, 'helvetica neue',
-               Ubuntu,
-               'segoe ui', arial,
-               sans-serif;
-}
-.page {
-  text-align: center;
-  /* nesting for the need to test postcss */
-  code {
-    background-color: #f0f0f0;
-    padding: 3px 5px;
-    border-radius: 2px;
-  }
-}
+/*@import "../../node_modules/normalize.css/normalize.css"*/
+@import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+
 </style>
